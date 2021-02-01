31b9a2a2be4fcd87a755c876fff40f81c60e1418
ludl4 <ludl4@lenovo.com>
2021-02-01 13:46:20 +0800

    初始化

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,51 +1,53 @@
 <template>
-    <el-container style="height: 500px; border: 1px solid #eee">
-        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-            <el-menu
-                    background-color="#545c64"
-                    text-color="#fff"
-                    active-text-color="#ffd04b"
-            >
-                <div>
-                    <h1>RPC</h1>
-                </div>
-                <el-submenu index="1">
-                    <template slot="title"><i class="el-icon-message"></i>凌拓数据处理</template>
-                    <!--                    <el-submenu index="1-4">-->
-                    <!--                        <template slot="title">选项4</template>-->
-                    <el-menu-item index="1-1">bom</el-menu-item>
-                    <el-menu-item index="1-2">jv服务注册</el-menu-item>
-                    <!--                    </el-submenu>-->
-                </el-submenu>
 
-            </el-menu>
-        </el-aside>
+        <el-container style="height: 500px; border: 1px solid #eee">
+            <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+                <el-menu
+                        background-color="#545c64"
+                        text-color="#fff"
+                        active-text-color="#ffd04b"
+                >
+                    <div>
+                        <h1>RPC</h1>
+                    </div>
+                    <el-submenu index="1">
+                        <template slot="title"><i class="el-icon-message"></i>凌拓数据处理</template>
+                        <!--                    <el-submenu index="1-4">-->
+                        <!--                        <template slot="title">选项4</template>-->
+                        <el-menu-item index="1-1">bom</el-menu-item>
+                        <el-menu-item index="1-2">jv服务注册</el-menu-item>
+                        <!--                    </el-submenu>-->
+                    </el-submenu>
 
-        <el-container>
-            <el-header style="text-align: right; font-size: 12px">
-                <el-dropdown>
-                    <i class="el-icon-setting" style="margin-right: 15px"></i>
-                    <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item>查看</el-dropdown-item>
-                        <el-dropdown-item>新增</el-dropdown-item>
-                        <el-dropdown-item>删除</el-dropdown-item>
-                    </el-dropdown-menu>
-                </el-dropdown>
-                <span>王一博</span>
-            </el-header>
+                </el-menu>
+            </el-aside>
 
-            <el-main>
-                <el-table :data="tableData">
-                    <el-table-column prop="date" label="日期" width="140">
-                    </el-table-column>
-                    <el-table-column prop="name" label="姓名" width="120">
-                    </el-table-column>
-                    <el-table-column prop="address" label="地址">
-                    </el-table-column>
-                </el-table>
-            </el-main>
+            <el-container>
+                <el-header style="text-align: right; font-size: 12px">
+                    <el-dropdown>
+                        <i class="el-icon-setting" style="margin-right: 15px"></i>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>查看</el-dropdown-item>
+                            <el-dropdown-item>新增</el-dropdown-item>
+                            <el-dropdown-item>删除</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                    <span>王一博</span>
+                </el-header>
+
+                <el-main>
+                    <el-table :data="tableData">
+                        <el-table-column prop="date" label="日期" width="140">
+                        </el-table-column>
+                        <el-table-column prop="name" label="姓名" width="120">
+                        </el-table-column>
+                        <el-table-column prop="address" label="地址">
+                        </el-table-column>
+                    </el-table>
+                </el-main>
+            </el-container>
         </el-container>
-    </el-container>
+
 </template>
 
 <script>
